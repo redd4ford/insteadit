50e166e11862476d662303f32d5466a18fc8dc23
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-08-18 16:21:24 +0300

    Added the corresponding repository for Comment class

---
+++ src/main/java/com/redd4ford/insteadit/repository/CommentRepository.java
@@ -0,0 +1,18 @@
+package com.redd4ford.insteadit.repository;
+
+import com.redd4ford.insteadit.model.Comment;
+import com.redd4ford.insteadit.model.Post;
+import com.redd4ford.insteadit.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+  List<Comment> findByPost(Post post);
+
+  List<Comment> findAllByUser(User user);
+
+}
