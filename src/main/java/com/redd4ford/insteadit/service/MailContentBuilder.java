28e6d20ea9a42c2a410a43ee9154079cd17b27e0
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-08-18 16:15:19 +0300

    Added a builder class to compose verification emails

---
+++ src/main/java/com/redd4ford/insteadit/service/MailContentBuilder.java
@@ -0,0 +1,21 @@
+package com.redd4ford.insteadit.service;
+
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class MailContentBuilder {
+  private final TemplateEngine templateEngine;
+
+  public MailContentBuilder(TemplateEngine templateEngine) {
+    this.templateEngine = templateEngine;
+  }
+
+  String build(String message) {
+    Context context = new Context();
+    context.setVariable("message", message);
+    return templateEngine.process("mailTemplate", context);
+  }
+
+}
