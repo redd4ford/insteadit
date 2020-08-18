d7ae06a1857d353ad481e0cbd13cc919120d9dd5
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-08-18 16:01:17 +0300

    Added a class to hold the app's constants

---
+++ src/main/java/com/redd4ford/insteadit/util/Constants.java
@@ -0,0 +1,10 @@
+package com.redd4ford.insteadit.util;
+
+public final class Constants {
+
+  public static final String ACTIVATION_EMAIL = "http://localhost:8080/api/auth/accountVerification";
+
+  private Constants() {
+  }
+
+}
