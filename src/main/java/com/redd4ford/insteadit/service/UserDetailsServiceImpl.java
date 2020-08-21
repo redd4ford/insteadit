6667fe4069845c75b8b86cfe43ee74aed7b3a4dd
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-08-21 14:30:43 +0300

    Added UserDetailsService implementation that fetches a user's information from the DB

---
+++ src/main/java/com/redd4ford/insteadit/service/UserDetailsServiceImpl.java
@@ -0,0 +1,44 @@
+package com.redd4ford.insteadit.service;
+
+import com.redd4ford.insteadit.model.User;
+import com.redd4ford.insteadit.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Collection;
+import java.util.Optional;
+
+import static java.util.Collections.singletonList;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  public UserDetailsServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Optional<User> userOptional = userRepository.findByUsername(username);
+    User user = userOptional.orElseThrow(() -> new UsernameNotFoundException("No user " +
+                                               "found with username : " + username));
+
+    return new org.springframework.security
+        .core.userdetails.User(user.getUsername(), user.getPassword(),
+        user.isEnabled(), true, true,
+        true, getAuthorities("USER"));
+  }
+
+  private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+    return singletonList(new SimpleGrantedAuthority(role));
+  }
+
+}
