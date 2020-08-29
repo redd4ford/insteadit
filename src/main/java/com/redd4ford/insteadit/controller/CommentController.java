45f0667cd06c24dc143a7c7a3ecd4aca47daf183
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-08-29 15:16:50 +0300

    Added CommentController

---
+++ src/main/java/com/redd4ford/insteadit/controller/CommentController.java
@@ -0,0 +1,40 @@
+package com.redd4ford.insteadit.controller;
+
+import com.redd4ford.insteadit.dto.CommentDto;
+import com.redd4ford.insteadit.service.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentController {
+
+  private final CommentService commentService;
+
+  public CommentController(CommentService commentService) {
+    this.commentService = commentService;
+  }
+
+  @GetMapping("/by-post/{id}")
+  public ResponseEntity<List<CommentDto>> getAllCommentsByPost(@PathVariable("id")
+                                                                       Long postId) {
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(commentService.getCommentsByPost(postId));
+  }
+
+  @GetMapping("/by-user/{username}")
+  public ResponseEntity<List<CommentDto>> getAllCommentsByUser(@PathVariable("username")
+                                                                      String username) {
+    return ResponseEntity.status(HttpStatus.OK).body(commentService.getCommentsByUser(username));
+  }
+
+  @PostMapping
+  public ResponseEntity<Void> createComment(@RequestBody CommentDto commentDto) {
+    commentService.save(commentDto);
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+
+}
