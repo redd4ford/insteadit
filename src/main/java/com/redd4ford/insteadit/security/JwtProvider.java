39fe493a930045c7feb15970a7270065bf5939f7
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-09-01 10:06:56 +0300

    Added token's expiration time

--- src/main/java/com/redd4ford/insteadit/security/JwtProvider.java
+++ src/main/java/com/redd4ford/insteadit/security/JwtProvider.java
@@ -3,6 +3,7 @@ package com.redd4ford.insteadit.security;
 import com.redd4ford.insteadit.exception.InsteaditException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.time.Instant;
+import java.util.Date;
 
 import static io.jsonwebtoken.Jwts.parser;
+import static java.util.Date.from;
+
+import static com.redd4ford.insteadit.util.Constants.JWT_EXPIRATION_IN_MILLS;
 
 @Service
 public class JwtProvider {
@@ -35,6 +41,17 @@ public class JwtProvider {
     User principal = (User) authentication.getPrincipal();
     return Jwts.builder()
         .setSubject(principal.getUsername())
+        .setIssuedAt(from(Instant.now()))
+        .setExpiration(Date.from(Instant.now().plusMillis(JWT_EXPIRATION_IN_MILLS)))
+        .signWith(getPrivateKey())
+        .compact();
+  }
+
+  public String generateTokenWithUserName(String username) {
+    return Jwts.builder()
+        .setSubject(username)
+        .setIssuedAt(from(Instant.now()))
+        .setExpiration(Date.from(Instant.now().plusMillis(JWT_EXPIRATION_IN_MILLS)))
         .signWith(getPrivateKey())
         .compact();
   }
@@ -49,11 +66,11 @@ public class JwtProvider {
   }
 
   public boolean validateToken(String jwt) {
-    parser().setSigningKey(getPublickey()).parseClaimsJws(jwt);
+    parser().setSigningKey(getPublicKey()).parseClaimsJws(jwt);
     return true;
   }
 
-  private PublicKey getPublickey() {
+  private PublicKey getPublicKey() {
     try {
       return keyStore.getCertificate("insteadit").getPublicKey();
     } catch (KeyStoreException e) {
@@ -63,7 +80,7 @@ public class JwtProvider {
 
   public String getUsernameFromJWT(String token) {
     Claims claims = parser()
-        .setSigningKey(getPublickey())
+        .setSigningKey(getPublicKey())
         .parseClaimsJws(token)
         .getBody();
     return claims.getSubject();
