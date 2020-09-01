bafd3243fca59e78fab93d23cc5a569e3765cf8a
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-09-01 10:14:25 +0300

    Added logout configurations

--- src/main/java/com/redd4ford/insteadit/config/SecurityConfig.java
+++ src/main/java/com/redd4ford/insteadit/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.redd4ford.insteadit.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,8 +15,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final UserDetailsService userDetailsService;
@@ -33,8 +37,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .authorizeRequests()
         .antMatchers("/api/auth/**")
         .permitAll()
+        .antMatchers("/")
+        .permitAll()
+
+        .and()
+
+        .authorizeRequests()
         .anyRequest()
-        .authenticated();
+        .authenticated()
+
+        .and()
+
+        .logout()
+        .logoutUrl("/api/auth/logout").logoutSuccessUrl("/")
+        .deleteCookies("JWT", "JSESSIONID")
+        .clearAuthentication(true)
+        .invalidateHttpSession(true)
+        .addLogoutHandler(new HeaderWriterLogoutHandler(
+            new ClearSiteDataHeaderWriter(ClearSiteDataHeaderWriter.Directive.ALL)));
 
     httpSecurity.addFilterBefore(jwtAuthenticationFilter,
         UsernamePasswordAuthenticationFilter.class);
@@ -46,8 +66,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     return super.authenticationManagerBean();
   }
 
+  @Bean
+  public LogoutSuccessHandler logoutSuccessHandler() {
+    return new LogoutSuccessHandler();
+  }
+
   @Autowired
-  public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+  public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder)
+      throws Exception {
     authenticationManagerBuilder.userDetailsService(userDetailsService)
         .passwordEncoder(passwordEncoder());
   }
