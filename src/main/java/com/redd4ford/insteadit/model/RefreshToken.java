40fa540e04d7a74066c9e97d7a7e289718caa971
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-09-01 10:09:32 +0300

    Added RefreshToken

---
+++ src/main/java/com/redd4ford/insteadit/model/RefreshToken.java
@@ -0,0 +1,52 @@
+package com.redd4ford.insteadit.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Entity
+public class RefreshToken {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String token;
+
+  private Instant createdDate;
+
+  public RefreshToken(String token, Instant createdDate) {
+    this.token = token;
+    this.createdDate = createdDate;
+  }
+
+  public RefreshToken() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+
+  public Instant getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(Instant createdDate) {
+    this.createdDate = createdDate;
+  }
+
+}
