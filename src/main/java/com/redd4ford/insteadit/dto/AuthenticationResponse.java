7ed1eeb1e39044183a6f968a0a5bfe053850bc9a
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-09-01 10:16:52 +0300

    Added refreshToken and AuthenticationResponseBuilder

--- src/main/java/com/redd4ford/insteadit/dto/AuthenticationResponse.java
+++ src/main/java/com/redd4ford/insteadit/dto/AuthenticationResponse.java
@@ -1,16 +1,27 @@
 package com.redd4ford.insteadit.dto;
 
+import java.time.Instant;
+
 public class AuthenticationResponse {
 
   private String authenticationToken;
   private String username;
+  private String refreshToken;
+  private Instant expiresAt;
 
-  public AuthenticationResponse(String authenticationToken, String username) {
+  public AuthenticationResponse(String authenticationToken, String username, String refreshToken,
+                                Instant expiresAt) {
     this.authenticationToken = authenticationToken;
     this.username = username;
+    this.refreshToken = refreshToken;
+    this.expiresAt = expiresAt;
   }
 
   public AuthenticationResponse() {
+  }
+
+  public static AuthenticationResponseBuilder builder() {
+    return new AuthenticationResponseBuilder();
   }
 
   public String getAuthenticationToken() {
@@ -29,4 +40,60 @@ public class AuthenticationResponse {
     this.username = username;
   }
 
+  public String getRefreshToken() {
+    return refreshToken;
+  }
+
+  public void setRefreshToken(String refreshToken) {
+    this.refreshToken = refreshToken;
+  }
+
+  public Instant getExpiresAt() {
+    return expiresAt;
+  }
+
+  public void setExpiresAt(Instant expiresAt) {
+    this.expiresAt = expiresAt;
+  }
+
+  public static class AuthenticationResponseBuilder {
+
+    private String authenticationToken;
+    private String username;
+    private String refreshToken;
+    private Instant expiresAt;
+
+    AuthenticationResponseBuilder() {
+    }
+
+    public AuthenticationResponseBuilder authenticationToken(String authenticationToken) {
+      this.authenticationToken = authenticationToken;
+      return this;
+    }
+
+    public AuthenticationResponseBuilder username(String username) {
+      this.username = username;
+      return this;
+    }
+
+    public AuthenticationResponseBuilder refreshToken(String refreshToken) {
+      this.refreshToken = refreshToken;
+      return this;
+    }
+
+    public AuthenticationResponseBuilder expiresAt(Instant expiresAt) {
+      this.expiresAt = expiresAt;
+      return this;
+    }
+
+    public AuthenticationResponse build() {
+      return new AuthenticationResponse(authenticationToken, username, refreshToken, expiresAt);
+    }
+
+    public String toString() {
+      return "AuthenticationResponse.AuthenticationResponseBuilder(authenticationToken="
+          + this.authenticationToken + ", username=" + this.username + ", refreshToken="
+          + this.refreshToken + ", expiresAt=" + this.expiresAt + ")";
+    }
+  }
 }
